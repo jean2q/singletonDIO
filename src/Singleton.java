32840f4dd9b0e7124c861293ec461438367d3b25
jean2q <pdelima.jeanlucas@gmail.com>
2022-05-31 14:46:17 -0300

    Simple singleton

---
+++ src/Singleton.java
@@ -0,0 +1,14 @@
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton(){
+        super();
+    }
+
+    public static Singleton getInstance(){
+        if (instance == null){
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
